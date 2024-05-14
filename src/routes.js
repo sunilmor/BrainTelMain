ec1bb2a9f740fa04a94cfbc1bc0494a4d9105d66
aloksteeprise <alok.singh@steeprise.com>
2024-05-14 18:51:10 +0530

    Add files via upload

--- src/routes.js
+++ src/routes.js
@@ -7,14 +7,14 @@ import RecorderPage from './component/Recorder/Recorderpage';
 import ResetPassword from './component/User/ResetPassword';
 import EmailVerification from './component/User/EmailVerification';
 import InstructionPage from  './component/User/InstructionPage'
-import RecorderPage_Old from './component/Recorder/Recorderpage1';
+// import RecorderPage_Old from './component/Recorder/Recorderpage1';
 
 const MainRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/record1" element={<RecorderPage_Old />} />
+      {/* <Route path="/record1" element={<RecorderPage_Old />} /> */}
       <Route path="/reset" element={<ResetPassword />} />
       <Route path="/verifyEmail" element={<EmailVerification />} />
       <Route path="/Instruction" element={<InstructionPage />} />
