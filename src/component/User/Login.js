0e9be0c1878d1c370b4a6dcfad83cbf95a5f644b
aloksteeprise <alok.singh@steeprise.com>
2024-05-14 19:05:55 +0530

    Add files via upload

--- src/component/User/Login.js
+++ src/component/User/Login.js
@@ -28,8 +28,10 @@ const Login = () => {
   const [password, setpassword] = React.useState('');
   const [error, setError] = React.useState('');
   const [page, setPage] = React.useState('login');
-
+ 
+  localStorage.clear();
   React.useEffect(() => {
+    
     setError(false);
   }, []);
 
