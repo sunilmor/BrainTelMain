98ac0fac9f926ec8026f44b4c7178ad8733702a5
aloksteeprise <alok.singh@steeprise.com>
2024-05-15 17:57:51 +0530

    Add files via upload

--- src/routes.js
+++ src/routes.js
@@ -7,18 +7,17 @@ import RecorderPage from './component/Recorder/Recorderpage';
 import ResetPassword from './component/User/ResetPassword';
 import EmailVerification from './component/User/EmailVerification';
 import InstructionPage from  './component/User/InstructionPage'
-// import RecorderPage_Old from './component/Recorder/Recorderpage1';
+import Protected from './service/isAuth';
 
 const MainRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/"  element={<Login />} />
       <Route path="/login" element={<Login />} />
-      {/* <Route path="/record1" element={<RecorderPage_Old />} /> */}
       <Route path="/reset" element={<ResetPassword />} />
       <Route path="/verifyEmail" element={<EmailVerification />} />
       <Route path="/Instruction" element={<InstructionPage />} />
-      <Route path="/record" element={<RecorderPage />} />
+      <Route path="/record" element={<Protected Component={RecorderPage} />} />
     </Routes>
   );
 };
